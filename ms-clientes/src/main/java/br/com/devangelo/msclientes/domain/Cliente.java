e902e897cde0713491244f94e1296f28b96548e2
Angelo A. Bezutti <angeloantoniobzt@gmail.com>
2022-08-12 16:04:57 -0300

    Add Domain e JPA

---
+++ ms-clientes/src/main/java/br/com/devangelo/msclientes/domain/Cliente.java
@@ -0,0 +1,27 @@
+package br.com.devangelo.msclientes.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data @NoArgsConstructor
+public class Cliente{
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String cpf;
+    @Column
+    private String nome;
+    @Column
+    private Integer idade;
+
+    public Cliente(String cpf, String nome, Integer idade) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
